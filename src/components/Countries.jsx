91833b32ee3e5c993e0be92a4fc6ab1a07d50c14
SofianAcDev <sofian.achbabi@gmail.com>
2023-02-22 20:10:04 +0100

    +Countries component

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 const Countries = () => {
+    const [data, setData] = useState([])
+    // const [hello, setHello] = useState("Hello les amis")
+
+    // le useEffect se joue lorsque le composant est monté
+    useEffect(()=>{
+axios.get("https://restcountries.com/v3.1/all")
+    .then((response)=>setData(response.data));
+    }, [])
     return (
         <div className='countries'>
-            COUNTRIES
+            <h1>COUNTRIES</h1>
+            <ul>
+                {/* quand on a pas de key unique, on passe un index */}
+                {data.map((country, index)=> <li key={index}>{country.name.common}</li>)}
+            </ul>
         </div>
     );
 };
